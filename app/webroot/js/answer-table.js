19c4d3fcdb9e624ff166d38710756ecf4f6dad00
biplob023051 <bcsarkar02@yahoo.com>
2015-05-12 18:42:10 +0600

    Auto save rate in 2 sec holding type

--- app/webroot/js/answer-table.js
+++ app/webroot/js/answer-table.js
@@ -17,14 +17,48 @@ function checkRow(row) {
 
     $('#overview').show();
     $('#details').hide();
-    
-    // update essay score
-    $('#details input').blur(function() {
+
+    $.fn.extend({
+        donetyping: function(callback,timeout){
+            timeout = timeout || 2e3; // 1 second default timeout
+            var timeoutReference,
+                doneTyping = function(el){
+                    if (!timeoutReference) return;
+                    timeoutReference = null;
+                    callback.call(el);
+                };
+            return this.each(function(i,el){
+                var $el = $(el);
+                // Chrome Fix (Use keyup over keypress to detect backspace)
+                // thank you @palerdot
+                $el.is(':input') && $el.on('keyup keypress',function(e){
+                    // This catches the backspace button in chrome, but also prevents
+                    // the event from triggering too premptively. Without this line,
+                    // using tab/shift+tab will make the focused element fire the callback.
+                    if (e.type=='keyup' && e.keyCode!=8) return;
+                    
+                    // Check if timeout has been set. If it has, "reset" the clock and
+                    // start over again.
+                    if (timeoutReference) clearTimeout(timeoutReference);
+                    timeoutReference = setTimeout(function(){
+                        // if we made it here, our timeout has elapsed. Fire the
+                        // callback
+                        doneTyping(el);
+                    }, timeout);
+                }).on('blur',function(){
+                    // If we can, fire the event since we're leaving the field
+                    doneTyping(el);
+                });
+            });
+        }
+    });
+
+    $('#details input').donetyping(function(){
         var marks = $(this).val();
         var max = $(this).attr("max");
         var current_score = $(this).attr("current-score"); 
         $("#ajax-message").hide();
-        
+
         if (marks < 0) {
             $("#ajax-message").removeClass('alert-success');
             $("#ajax-message").addClass('alert-danger');
@@ -82,6 +116,72 @@ function checkRow(row) {
             }
         });
     });
+
+    
+    // update essay score
+    // $('#details input').blur(function() {
+    //     var marks = $(this).val();
+    //     var max = $(this).attr("max");
+    //     var current_score = $(this).attr("current-score"); 
+    //     $("#ajax-message").hide();
+        
+    //     if (marks < 0) {
+    //         $("#ajax-message").removeClass('alert-success');
+    //         $("#ajax-message").addClass('alert-danger');
+    //         $("#ajax-message").show();
+    //         $("#ajax-message").html('Please Give a postive number!');
+    //         $('html, body').animate({
+    //             scrollTop: $(".page-header").offset().top
+    //         }, 500);
+    //         return false;
+    //     } else if (marks == current_score) {
+    //         $("#ajax-message").removeClass('alert-success');
+    //         $("#ajax-message").addClass('alert-danger');
+    //         $("#ajax-message").show();
+    //         $("#ajax-message").html('You have not updated score yet!');
+    //         $('html, body').animate({
+    //             scrollTop: $(".page-header").offset().top
+    //         }, 500);
+    //         return false;
+    //     } else if (marks > max) {
+    //         $("#ajax-message").removeClass('alert-success');
+    //         $("#ajax-message").addClass('alert-danger');
+    //         $("#ajax-message").show();
+    //         $("#ajax-message").html('Points not allowed more than ' + max + ' value');
+    //         $('html, body').animate({
+    //             scrollTop: $(".page-header").offset().top
+    //         }, 500);
+    //         return false;
+    //     } 
+
+    //     $(this).attr("current-score", marks);
+
+    //     var std_id = $(this).attr("name");
+    //     var q_id = $(this).attr("question");
+
+
+    //     $.ajax({
+    //         dataType: 'json',
+    //         url: appData.baseUrl + '/score/update',
+    //         type: 'post',
+    //         data: {'id': q_id, 'student_id': std_id, 'score': marks, 'current_score' : current_score, 'max' : max},
+    //         success: function (response)
+    //         {
+    //             console.log(response);
+    //             if (response.success || response.success === "true")
+    //             {
+    //                 $("#studentscr2-" + std_id).text(response.score);
+    //                 $("#studentscr1-" + std_id).text(response.score);
+    //                 $("#ajax-message").removeClass('alert-danger');
+    //                 $("#ajax-message").addClass('alert-success');
+    //                 $("#ajax-message").show();
+    //                 $("#ajax-message").html('Points updated successfully');
+    //             } else {
+    //                 alert('Something went wrong, try again later');
+    //             }
+    //         }
+    //     });
+    // });
 
     var appData = $.parseJSON($("#app-data").text());
 
