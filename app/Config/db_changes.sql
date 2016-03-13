a690c591dd8c127357b11e2a71ad1ebb2527b8d6
biplob023051 <bcsarkar02@yahoo.com>
2016-03-13 15:36:02 +0600

    Db changes

--- app/Config/db_changes.sql
+++ app/Config/db_changes.sql
@@ -1,2 +1,4 @@
 ALTER TABLE `students` ADD `status` TINYINT(1) NULL DEFAULT NULL COMMENT 'Null for incomplete, 1 for complete' AFTER `class`;
 ALTER TABLE `questions` ADD `max_allowed` TINYINT(2) NULL DEFAULT NULL AFTER `weight`;
+
+UPDATE students SET `status`=1;
